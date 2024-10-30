17f5e6a6a256bad655a58a17f4cd9bb715bd6e76
Shraddha Bhavsar <shraddhabhavsar1807@gmail.com>
2024-10-30 19:30:03 +0530

    Done third task - Password generator

---
+++ Task3_Password_generator.py
@@ -0,0 +1,19 @@
+import string
+import random
+
+available_characters = string.ascii_letters + string.digits + string.punctuation
+
+while True:
+    password_length = input("Enter the password length: ")
+    try:
+        password_length = int(password_length)
+    except ValueError:
+        print("Invalid input, only numbers are accepted. Please try again")
+    else:
+        if 8 <= password_length <= 50:
+            break
+        print("Invalid input, the password length should be between 8 and 50: ")
+
+generated_password = "".join([random.choice(available_characters) for i in range(password_length)])
+
+print(f"\nYour generated password is: {generated_password}")
