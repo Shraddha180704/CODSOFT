214bc183b673d49c42929f4bd6a1b2e58c767723
Shraddha Bhavsar <shraddhabhavsar1807@gmail.com>
2024-11-19 22:35:22 +0530

    Updated task1

--- Task1_To do list.py
+++ Task1_To do list.py
@@ -1,23 +1,24 @@
 import sys
 todo_list = []
 def display_menu():
-
     print("\nMENU:")
     print("1. Create new task")
     print("2. Update existing task")
     print("3. Track all tasks")
     print("4. Exit\n")
-    option_no = 0
-    while option_no not in [1, 2, 3, 4]:
-        option_no = int(input("Please enter the option number(1/2/3/4): "))
-    if option_no == 1:
-        create_new_task()
-    elif option_no == 2:
-        update_existing_task()
-    elif option_no == 3:
-        track_all_tasks()
-    else:
-        sys.exit()
+
+    while True:
+        option_no = input("Please enter the option number(1/2/3/4): ")
+        if option_no == "1":
+            create_new_task()
+        elif option_no == "2":
+            update_existing_task()
+        elif option_no == "3":
+            track_all_tasks()
+        elif option_no == "4":
+            sys.exit()
+        else:
+            print("Invalid input")
 
 
 def create_new_task():
@@ -37,7 +38,12 @@ def update_existing_task():
     track_all_tasks(True)
     update_task_no = 0
     while not 0 < update_task_no <= len(todo_list):
-        update_task_no = int(input("To update existing task, please enter the number: "))
+        update_task_no = input("To update existing task, please enter the number: ")
+        if update_task_no.isdigit():
+            update_task_no = int(update_task_no)
+        else:
+            update_task_no = 0
+            print("please enter the number")
     updated_task = input(f"Enter new data to update task number {update_task_no}: ")
     todo_list[update_task_no - 1] = updated_task
     print("Successfully updated existing task",update_task_no)
